d7c1f29f23a55b7c2e710b95d2412bd8dc2300da
hashimon <hashimon.hashimon@icloud.com>
2022-09-03 16:21:46 +0900

    first commit

--- main.py
+++ main.py
@@ -31,5 +31,5 @@ import numpy as np
 uploaded_file = st.file_uploader("ファイルアップロード", type='jpg')
 image=Image.open(uploaded_file)
 img_array = np.array(image)
-st.botton("押してください")
+st.button("押してください")
 st.image(img_array,caption = 'サムネイル画像',use_column_width = True)
