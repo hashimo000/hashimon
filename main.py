6f17c1dfba6be632dfd239b39c611e8f0ae4286c
hashimon <hashimon.hashimon@icloud.com>
2022-09-03 15:06:20 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.title("Streamlit 始めてみました")
+st.write("こんにちは！")
+"""
+#ありがとう
+##こんにちは
+###こんばんは
+"""
+df = pd.DataFrame({
+"1st":[1,2,3,4],
+"2nd":[10,11,12,13]
+})
+st.dataframe(df,width=300,height=300)
+
+chart_data = pd.DataFrame(
+    np.random.randn(20,3),
+    columns=["a","b","c"]
+)
+st.line_chart(chart_data)
+
+df = pd.DataFrame(
+    np.random.rand(100,2)/[50,50] + [34.6,135.5],
+    columns=["lat","lon"]
+)
+st.map(df)
