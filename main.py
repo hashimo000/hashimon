208cf2e702d08f39d2553063830881169cc6d1f7
hashimon <hashimon.hashimon@icloud.com>
2022-09-03 16:15:18 +0900

    first

--- main.py
+++ main.py
@@ -28,4 +28,7 @@ st.map(df)
 import streamlit as st
 from PIL import Image
 import numpy as np
-st.file_uploader("ファイルアップロード", type='jpg')
+uploaded_file = st.file_uploader("ファイルアップロード", type='jpg')
+image=Image.open(uploaded_file)
+img_array = np.array(image)
+st.image(img_array,caption = 'サムネイル画像',use_column_width = True)
