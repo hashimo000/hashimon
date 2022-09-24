da357d0ffae15856e46bccc1c64cd5bd2cd92c16
hashimon <hashimon.hashimon@icloud.com>
2022-09-24 13:54:01 +0900

    no message

--- main.py
+++ main.py
@@ -46,3 +46,15 @@ text = st.sidebar.text_input('あなたの趣味を教えてください。')
 'あなたの趣味：', text
 condition = st.sidebar.slider('あなたの今の調子は？',0,10,5)
 'コンディション:',condition
+
+left_colum, right_column = st.beta_columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右コラム')
+
+expander1=st.beta_expander('問い合わせ１')
+expander1.write('問い合わせ１の回答')
+expander2=st.beta_expander('問い合わせ２')
+expander2.write('問い合わせ２の回答')
+expander3=st.beta_expander('問い合わせ３')
+expander3.write('問い合わせ３の回答')
